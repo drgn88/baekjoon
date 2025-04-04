dafa66ed1135cbe9a7913602100f88a1d44834c8
drgn88 <135236975+drgn88@users.noreply.github.com>
2025-04-04 17:06:11 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int char_size(char arr[]);
+char S[1000001] = { 0 };
+
+int main(void)
+{
+	
+	int num;
+	int i;
+	scanf("%[^\n]s", S);
+	num = char_size(S);
+
+	int cnt = 0;
+	for (i = 0; i < num; i++)
+	{
+		if (S[i] == ' ')
+		{
+			cnt++;
+		}
+	}
+	
+	
+	if (S[0] == ' ') cnt--;
+	if (S[num-1] == ' ') cnt--;
+	cnt++;
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
+
+int char_size(char arr[])
+{
+	int i;
+	int check = 0;
+	for (i = 0;; i++)
+	{
+		if (arr[i] == 0)
+		{
+			return check;
+		}
+		check++;
+	}
+}
